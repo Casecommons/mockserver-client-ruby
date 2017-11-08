a9dfa560a67c8615012f9e145b3c2d63a7faaaef
Andrew Marshall <amarshall@casecommons.org>
2017-11-08 15:33:52 -0500

    OPTIONS is a valid HTTP method

--- lib/mockserver/model/request.rb
+++ lib/mockserver/model/request.rb
@@ -14,7 +14,7 @@ module MockServer::Model
   # Enum for HTTP methods
   class HTTPMethod < SymbolizedEnum
     def allowed_values
-      [:GET, :POST, :PUT, :DELETE, :PATCH]
+      [:GET, :POST, :PUT, :DELETE, :PATCH, :OPTIONS]
     end
   end
 
@@ -24,7 +24,7 @@ module MockServer::Model
     include Hashie::Extensions::IgnoreUndeclared
     include Hashie::Extensions::Coercion
 
-    ALLOWED_METHODS = [:GET, :POST, :PUT, :DELETE, :PATCH]
+    ALLOWED_METHODS = [:GET, :POST, :PUT, :DELETE, :PATCH, :OPTIONS]
 
     property :method, required: true, default: :GET
     property :path, required: true, default: ''
